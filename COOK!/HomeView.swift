428686d3b755b6ff7919e88aebd3fdc69ac633c8
Colin Butera <ccmjbutera@gmail.com>
2025-10-08 15:40:13 -0400

    fixed issues with file conflicts

---
+++ COOK!/HomeView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  COOK!
+//
+//  Created by Alexa Witkin on 10/7/25.
+//
+
+
+import SwiftUI
+
+struct HomeView: View {
+  var body: some View {
+      Text("COOK")
+      
+      
+    TabView {
+      GroceryListView()
+        .tabItem() {
+          Image(systemName: "list.dash")
+        }
+      IngredientsView()
+        .tabItem() {
+          Image(systemName: "cabinet")
+        }
+      PlusView()
+        .tabItem() {
+          Image(systemName: "plus")
+        }
+      RecipesView()
+        .tabItem {
+          Image(systemName: "bookmark")
+        }
+      ScheduleView()
+        .tabItem {
+          Image(systemName: "calendar")
+        }
+    }
+  }
+}
+
+#Preview {
+  HomeView()
+}
+
+
+
+extension View {
+  func bottomLine() -> some View {
+    GeometryReader { geometry in
+      self
+        .frame(width: geometry.size.width, height: geometry.size.height).border(.black)
+    }      .ignoresSafeArea(.all, edges: .top)
+  }
+}
