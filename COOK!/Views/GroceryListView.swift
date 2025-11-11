b15f099ac2f8fd4e907ca53df9048659a70fa37a
szimpfer <samzimpfer1@gmail.com>
2025-11-11 10:13:25 -0500

    Grocery list edits

--- COOK!/Views/GroceryListView.swift
+++ COOK!/Views/GroceryListView.swift
@@ -47,103 +47,104 @@ struct GroceryListView: View {
                     deletable: true
                 )
                 
-                // Clear and add buttons
-                HStack(spacing: 8) {
-                    // Clear Button
+                Spacer()
+                
+                VStack() {
                     Button(action: {
-                        showClearMenu = true
+                        appState.addToPantry(appState.selectedGroceryItems)
                     }) {
-                        Text("Clear")
-                            .frame(maxWidth: .infinity)
+                        Text("Add selected to pantry").frame(maxWidth: .infinity)
                     }
                     .buttonStyle(.borderedProminent)
-                    // Context menu for clearing
-                    .sheet(isPresented: $showClearMenu) {
-                        VStack {
-                            Button(action: {
-                                appState.addToPantry(appState.selectedGroceryItems)
-                                showClearMenu = false
-                            }) {
-                                Text("Add selected to pantry").frame(maxWidth: .infinity)
-                            }
-                            .buttonStyle(.borderedProminent)
-                            
-                            Button(action: {
-                                appState.removeFromGroceryList(appState.selectedGroceryItems)
-                                appState.selectedGroceryItems = []
-                                showClearMenu = false
-                            }) {
-                                Text("Clear selected").frame(maxWidth: .infinity)
-                            }
-                            .buttonStyle(.borderedProminent)
-                            
-                            Button(action: {
-                                appState.groceryList = []
-                                showClearMenu = false
-                            }) {
-                                Text("Clear all").frame(maxWidth: .infinity)
-                            }
-                            .buttonStyle(.borderedProminent)
-                            
-                            Button(action: { showClearMenu = false }) {
-                                Text("Cancel").frame(maxWidth: .infinity)
-                            }
-                            .buttonStyle(.borderedProminent)
-                        }
-                        .presentationDetents([.height(250)])
-                        .padding(.horizontal, 50)
-                    }
                     
-                    // Add Item button updates showAddItemMenu
-                    Button(action: {
-                        showAddItemMenu = true
-                        quantity = 0
-                    }) {
-                        Text("Add Item")
-                            .frame(maxWidth: .infinity)
-                    }
-                    .buttonStyle(.borderedProminent)
-                    // Context menu for adding an item
-                    .sheet(isPresented: $showAddItemMenu) {
-                        VStack() {
-                            TextField(
-                                "Item Name",
-                                text: $itemToAdd)
-                            .textFieldStyle(.roundedBorder)
-                                .padding()
-                            Stepper("Quantity: \(quantity)", value: $quantity, in: 0...100)
-                                .padding()
- 
-                            HStack {
-                                // Cancel Button
-                                Button(action: {
-                                    showAddItemMenu = false
-                                    itemToAdd = ""
+                    // Clear and add buttons
+                    HStack(spacing: 8) {
+                        // Clear Button
+                        Button(action: {
+                            showClearMenu = true
+                        }) {
+                            Text("Clear")
+                                .frame(maxWidth: .infinity)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        // Context menu for clearing
+                        .sheet(isPresented: $showClearMenu) {
+                            VStack {Button(action: {
+                                    appState.removeFromGroceryList(appState.selectedGroceryItems)
+                                    appState.selectedGroceryItems = []
+                                    showClearMenu = false
                                 }) {
-                                    Text("Cancel")
-                                        .frame(maxWidth: .infinity)
+                                    Text("Clear selected").frame(maxWidth: .infinity)
                                 }
                                 .buttonStyle(.borderedProminent)
-                                // Add Item button
+                                
                                 Button(action: {
-                                    if !itemToAdd.isEmpty {
-                                        appState.addToGroceryList(name: itemToAdd, quantity: quantity)
+                                    appState.groceryList = []
+                                    showClearMenu = false
+                                }) {
+                                    Text("Clear all").frame(maxWidth: .infinity)
+                                }
+                                .buttonStyle(.borderedProminent)
+                                
+                                Button(action: { showClearMenu = false }) {
+                                    Text("Cancel").frame(maxWidth: .infinity)
+                                }
+                                .buttonStyle(.borderedProminent)
+                            }
+                            .presentationDetents([.height(250)])
+                            .padding(.horizontal, 50)
+                        }
+                        
+                        // Add Item button updates showAddItemMenu
+                        Button(action: {
+                            showAddItemMenu = true
+                            quantity = 0
+                        }) {
+                            Text("Add Item")
+                                .frame(maxWidth: .infinity)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        // Context menu for adding an item
+                        .sheet(isPresented: $showAddItemMenu) {
+                            VStack() {
+                                TextField(
+                                    "Item Name",
+                                    text: $itemToAdd)
+                                .textFieldStyle(.roundedBorder)
+                                    .padding()
+                                Stepper("Quantity: \(quantity)", value: $quantity, in: 0...100)
+                                    .padding()
+     
+                                HStack {
+                                    // Cancel Button
+                                    Button(action: {
                                         showAddItemMenu = false
                                         itemToAdd = ""
+                                    }) {
+                                        Text("Cancel")
+                                            .frame(maxWidth: .infinity)
                                     }
-                                }) {
-                                    Text("Add")
-                                        .frame(maxWidth: .infinity)
+                                    .buttonStyle(.borderedProminent)
+                                    // Add Item button
+                                    Button(action: {
+                                        if !itemToAdd.isEmpty {
+                                            appState.addToGroceryList(name: itemToAdd, quantity: quantity)
+                                            showAddItemMenu = false
+                                            itemToAdd = ""
+                                        }
+                                    }) {
+                                        Text("Add")
+                                            .frame(maxWidth: .infinity)
+                                    }
+                                    .buttonStyle(.borderedProminent)
                                 }
-                                .buttonStyle(.borderedProminent)
                             }
-                            .padding(.horizontal)
+                            .presentationDetents([.height(200)])
                         }
-                        .presentationDetents([.height(200)])
                     }
                 }
-                .tint(pink)
                 .padding(.horizontal)
+                .tint(pink)
                 
                 Spacer()
             }
