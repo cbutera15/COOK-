a8ecc45906ba8ad619524553dabeb437f54f0bc6
szimpfer <samzimpfer1@gmail.com>
2025-11-19 22:33:04 -0500

    Started working on upcoming meals

--- COOK!/Views/HomeView.swift
+++ COOK!/Views/HomeView.swift
@@ -10,9 +10,29 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var appState: AppState
     
+    // TODO replace today with actual calculation of today and pull info from appState.schedule
+    @State var today: Day = Day(
+        id: 0,
+        name: "Monday",
+        meals: [
+            Recipe(name: "Chicken and rice"),
+            Recipe(name: "Pasta salad")
+        ]
+    )
+    @State var showAddRecipe: Bool = false
+    @State var selectedDay: String = ""
+    
     var body: some View {
         VStack {
             Text("Upcoming Meals")
+            DayView(
+                day: $today,
+                showAddRecipe: $showAddRecipe,
+                selectedDay: $selectedDay,
+                color: .gray,
+                buttonColor: .white,
+                showDelete: false
+            )
             Spacer()
             Text("Favorite Meals")
             Spacer()
